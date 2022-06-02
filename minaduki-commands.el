fe026cd002e55063f198ecb1e49e4102fe89a396
Kisaragi Hiu <mail@kisaragi-hiu.com>
2022-06-02 21:37:42 +0900

    Move org-specific local commands to their own module

--- minaduki-commands.el
+++ minaduki-commands.el
@@ -21,69 +21,10 @@
 (require 'minaduki-utils)
 (require 'kisaragi-notes-templates)
 
-
 (require 'minaduki-extract)
 (require 'org-roam-capture)
 
 ;;;; Local commands
-
-(defun minaduki/org-heading-to-file//suffix (&optional dir full? visit?)
-  "Write the current heading to a file under DIR.
-
-DIR defaults to current directory (`default-directory').
-
-The name of the created file is based on the heading. By default,
-this is the first WORD of the heading; if FULL? is non-nil, this
-happens:
-
-- take the entire heading
-- dashes and colons are removed,
-- then spaces are replaced with dashes,
-- and everything is turned into lowercase (except the T in a timestamp).
-
-For example, given a heading \"2020-05-29T00:00:00+0800 my heading\",
-when FULL? is non-nil the file name will be
-\"20200529T000000+0800-my-heading.org\", otherwise it will be
-\"20200529T000000+0800.org\".
-
-When VISIT? is non-nil, visit the new file after creating it.
-
-Interactively, please use the transient command instead."
-  (interactive (let ((args (transient-args 'minaduki/org-heading-to-file)))
-                 (transient-save)
-                 (list (transient-arg-value "--dir=" args)
-                       (transient-arg-value "--full" args)
-                       (transient-arg-value "--open" args))))
-  (let* ((dir (or dir default-directory))
-         (title (org-entry-get nil "ITEM"))
-         (filename (->> (if full?
-                            title
-                          (car (s-split " " title)))
-                        (s-replace-regexp (rx (any "-/,:?\"!'\\")) "")
-                        (s-replace-regexp " +" "-")
-                        downcase
-                        (s-replace-regexp (rx (group digit) "t" (group digit))
-                                          "\\1T\\2")
-                        (format "%s.org")))
-         (path (f-join dir filename))
-         (content (save-mark-and-excursion
-                    (org-mark-subtree)
-                    (buffer-substring-no-properties
-                     (region-beginning)
-                     (region-end)))))
-    (with-temp-file path
-      (insert content))
-    (when visit?
-      (find-file path))))
-
-(transient-define-prefix minaduki/org-heading-to-file ()
-  "Export heading at point to a file."
-  ["Options"
-   ("-d" "Directory to export to" "--dir=" transient-read-directory)
-   ("-f" "Use the entire heading instead of just the first WORD" "--full")
-   ("-v" "Open the exported file" "--open")]
-  ["Command"
-   ("e" "Export" minaduki/org-heading-to-file//suffix)])
 
 (cl-defun minaduki/insert (&key entry lowercase? region)
   "Insert a link to a note.
