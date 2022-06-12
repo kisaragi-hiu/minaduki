735150241170cb720be4d52d53c4d3df314ec684
Kisaragi Hiu <mail@kisaragi-hiu.com>
2022-06-13 02:36:49 +0900

    Use Obsidian-style links by default, not just in Obsidian vaults

--- minaduki-markdown.el
+++ minaduki-markdown.el
@@ -14,14 +14,13 @@
 (defun minaduki-markdown-follow (&optional other)
   "Follow thing at point.
 
-Like `markdown-follow-thing-at-point', but add support for Obsidian links.
+Like `markdown-follow-thing-at-point', but with support for Obsidian links.
 
 When OTHER is non-nil (with a \\[universal-argument]),
 open in another window instead of in the current one."
   (interactive "P")
-  (if (and (minaduki--in-obsidian-vault?)
-           (let ((markdown-enable-wiki-links t))
-             (markdown-wiki-link-p)))
+  (if (let ((markdown-enable-wiki-links t))
+        (markdown-wiki-link-p))
       (let ((path (minaduki-obsidian-path (match-string 3))))
         (when other (other-window 1))
         (minaduki//find-file path))
